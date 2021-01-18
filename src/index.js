e15d1e91f61e32bc4748608f7ff63f79cb5e06d1
NG <you@example.comng@ng.com>
2021-01-18 17:46:11 +0100

    Small library created

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var uniqueRandomArray = require('unique-random-array');
+var starWarNames = require('./starwars-names.json');
+
+
+module.exports={
+
+  all: starWarNames,
+  random: uniqueRandomArray(starWarNames)
+
+};
